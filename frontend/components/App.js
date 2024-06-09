f129d562fc72d53a915458e3fc77a3a2822c0ccb
Christopher Ralph <chris.734c@gmail.com>
2024-06-09 11:25:53 -0400

    Extracted the error response handling into a helper method and put the form reset into a helper method.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -20,19 +20,25 @@ export default class App extends React.Component {
     this.setState({ ...this.state.newTodoName, newTodoName: value });
   };
 
+  updateApiResponseError = (err) => {
+    this.setState({
+      ...this.state.error,
+      error: err.response.data.message,
+    });
+  };
+  
+  resetForm = () => {
+    this.setState({ ...this.state, todoName: "" });
+  };
+
   postTodo = async () => {
     axios
       .post(URL, { name: this.state.newTodoName })
       .then((res) => {
         this.getTodos();
-        this.setState({ ...this.state, todoName: "" });
+        this.resetForm();
       })
-      .catch((err) => {
-        this.setState({
-          ...this.state.error,
-          error: err.response.data.message,
-        });
-      });
+      .catch(this.updateApiResponseError);
   };
 
   onTodoSubmit = (event) => {
@@ -46,18 +52,11 @@ export default class App extends React.Component {
       .then((res) => {
         this.setState({ ...this.state, todos: res.data.data });
       })
-      .catch((err) => {
-        this.setState({
-          ...this.state.error,
-          error: err.response.data.message,
-        });
-      });
+      .catch(this.updateApiResponseError);
   };
 
   componentDidMount() {
     this.getTodos();
-    // const todosResponse = this.getTodos();
-    // this.setState({ todos: todosResponse });
   }
 
   render() {
