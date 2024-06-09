657676fe660ffeed41b02501dd50c18d8bdafb25
Christopher Ralph <chris.734c@gmail.com>
2024-06-09 11:16:34 -0400

    Implemented a postTodo function and tied it to the form's submit.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -10,15 +10,35 @@ export default class App extends React.Component {
     super();
     this.state = {
       todos: [],
-      error: '',
-      todoName: '',
+      error: "",
+      newTodoName: "",
     };
   }
 
-  handleTodoNameInput = event => {
-    const { value } = event.target
-    this.setState({ ...this.state.todoName, todoName: value })
-  }
+  handleTodoNameInput = (event) => {
+    const { value } = event.target;
+    this.setState({ ...this.state.newTodoName, newTodoName: value });
+  };
+
+  postTodo = async () => {
+    axios
+      .post(URL, { name: this.state.newTodoName })
+      .then((res) => {
+        this.getTodos();
+        this.setState({ ...this.state, todoName: "" });
+      })
+      .catch((err) => {
+        this.setState({
+          ...this.state.error,
+          error: err.response.data.message,
+        });
+      });
+  };
+
+  onTodoSubmit = (event) => {
+    event.preventDefault();
+    this.postTodo();
+  };
 
   getTodos = async () => {
     axios
@@ -51,10 +71,17 @@ export default class App extends React.Component {
         {this.state.todos.map((todo) => {
           return <div key={todo.id}>{todo.name}</div>;
         })}
-        <input value={this.state.todoName} type="text" placeholder="Type a todo" onChange={this.handleTodoNameInput} />
-        <button>Submit</button> <br />
-        <br />
-        <button>Hide Completed</button>
+        <form onSubmit={this.onTodoSubmit}>
+          <input
+            value={this.state.todoName}
+            type="text"
+            placeholder="Type a todo"
+            onChange={this.handleTodoNameInput}
+          />
+          <button type="submit">Submit</button> <br />
+          <br />
+          <button>Hide Completed</button>
+        </form>
       </div>
     );
   }
